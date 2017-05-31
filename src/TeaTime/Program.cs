9e0c870f1be22f2d36a3fa7257ec2ca1c2bec8ca
MrSmoke <syco711@gmail.com>
2017-05-31 22:25:44 +1000

    Added AspNetCore app

---
+++ src/TeaTime/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TeaTime
+{
+    using System.IO;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .UseUrls("http://*:5000")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
