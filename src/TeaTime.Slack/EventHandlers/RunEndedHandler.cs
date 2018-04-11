79d88b8d033ffc5c947dd033f02d93e068867821
MrSmoke <syco711@gmail.com>
2018-04-11 15:03:58 +1000

    Fixed the runner showing the wrong user

--- src/TeaTime.Slack/EventHandlers/RunEndedHandler.cs
+++ src/TeaTime.Slack/EventHandlers/RunEndedHandler.cs
@@ -1,9 +1,14 @@
 ﻿namespace TeaTime.Slack.EventHandlers
 {
+    using System.Linq;
+    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using Client;
+    using Common.Abstractions.Data;
     using Common.Features.Runs.Events;
+    using Common.Models;
+    using Common.Models.Domain;
     using MediatR;
     using Models.Responses;
     using Resources;
@@ -11,10 +16,14 @@
     internal class RunEndedHandler : INotificationHandler<RunEndedEvent>
     {
         private readonly ISlackApiClient _slackApiClient;
+        private readonly ILinkRepository _linkRepository;
+        private readonly IUserRepository _userRepository;
 
-        public RunEndedHandler(ISlackApiClient slackApiClient)
+        public RunEndedHandler(ISlackApiClient slackApiClient, ILinkRepository linkRepository, IUserRepository userRepository)
         {
             _slackApiClient = slackApiClient;
+            _linkRepository = linkRepository;
+            _userRepository = userRepository;
         }
 
         public async Task Handle(RunEndedEvent notification, CancellationToken cancellationToken)
@@ -22,15 +31,41 @@
             if (!notification.TryGetCallbackState(out var callbackData))
                 return;
 
-            var message =
-                $"Congratulations {StringHelpers.SlackUserId(callbackData.UserId)} you drew the short straw :cup_with_straw: Here's the order: ";
+            var runnerSlackId = await _linkRepository.GetLinkAsync(notification.RunnerUserId, LinkType.User).ConfigureAwait(false);
 
-            foreach (var o in notification.Orders)
+            string slackRunnerIdStr;
+            if (string.IsNullOrWhiteSpace(runnerSlackId))
             {
-                message += $"\n{o.User.DisplayName}: {o.Option.Name}";
+                var runner = await _userRepository.GetAsync(notification.RunnerUserId).ConfigureAwait(false);
+                slackRunnerIdStr = runner.DisplayName;
+            }
+            else
+            {
+                slackRunnerIdStr = StringHelpers.SlackUserId(runnerSlackId);
             }
 
-            var data = new SlashCommandResponse(message, ResponseType.Channel);
+            var messageBuilder = new StringBuilder();
+
+            messageBuilder
+                .Append("Congratulations ")
+                .Append(slackRunnerIdStr)
+                .Append(" you drew the short straw :cup_with_straw:. Here's the order:")
+                .Append("\n```\n");
+
+            var orders = notification.Orders.ToList();
+
+            foreach (var o in orders)
+            {
+                messageBuilder
+                    .Append(o.User.DisplayName)
+                    .Append(": ")
+                    .Append(o.Option.Name)
+                    .Append("\n");
+            }
+
+            messageBuilder.Append("```");
+
+            var data = new SlashCommandResponse(messageBuilder.ToString(), ResponseType.Channel);
 
             await _slackApiClient.PostResponseAsync(callbackData.ResponseUrl, data).ConfigureAwait(false);
         }
