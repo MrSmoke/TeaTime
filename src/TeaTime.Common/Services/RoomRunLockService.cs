dcf37f40e0ed5ae022389caff842b8b2ebd37ce1
MrSmoke <syco711@gmail.com>
2019-04-20 13:52:28 +1000

    Adding some logging around lock creation

--- src/TeaTime.Common/Services/RoomRunLockService.cs
+++ src/TeaTime.Common/Services/RoomRunLockService.cs
@@ -3,24 +3,37 @@
     using System.Threading.Tasks;
     using Abstractions;
     using Abstractions.Data;
+    using Microsoft.Extensions.Logging;
 
     public class RoomRunLockService : IRoomRunLockService
     {
         private readonly ILockRepository _lockRepository;
+        private readonly ILogger<RoomRunLockService> _logger;
 
-        public RoomRunLockService(ILockRepository lockRepository)
+        public RoomRunLockService(ILockRepository lockRepository, ILogger<RoomRunLockService> logger)
         {
             _lockRepository = lockRepository;
+            _logger = logger;
         }
 
-        public Task<bool> CreateLockAsync(long roomId)
+        public async Task<bool> CreateLockAsync(long roomId)
         {
-            return _lockRepository.CreateLockAsync(GetLockKey(roomId));
+            var key = GetLockKey(roomId);
+            var result = await _lockRepository.CreateLockAsync(key);
+
+            _logger.LogDebug("Create run lock for room {RoomId} with key {Key} result = {Result}", roomId, key, result);
+
+            return result;
         }
 
-        public Task<bool> DeleteLockAsync(long roomId)
+        public async Task<bool> DeleteLockAsync(long roomId)
         {
-            return _lockRepository.DeleteLockAsync(GetLockKey(roomId));
+            var key = GetLockKey(roomId);
+            var result = await _lockRepository.DeleteLockAsync(key);
+
+            _logger.LogDebug("Delete run lock for room {RoomId} with key {Key} result = {Result}", roomId, key, result);
+
+            return result;
         }
 
         private static string GetLockKey(long roomId)
