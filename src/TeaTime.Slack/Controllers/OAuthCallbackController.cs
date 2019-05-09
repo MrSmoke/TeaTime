3d2eddcd594b03b29a7b9fca0c481ff1f1c4f564
MrSmoke <syco711@gmail.com>
2019-05-09 21:17:25 +1000

    Add some logging

--- src/TeaTime.Slack/Controllers/OAuthCallbackController.cs
+++ src/TeaTime.Slack/Controllers/OAuthCallbackController.cs
@@ -1,9 +1,11 @@
 ﻿namespace TeaTime.Slack.Controllers
 {
+    using System;
     using System.Threading.Tasks;
     using Client;
     using Configuration;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
     using Models.Requests;
 
@@ -11,25 +13,33 @@
     {
         private readonly ISlackApiClient _slackApiClient;
         private readonly IOptionsMonitor<SlackOptions> _optionsAccessor;
+        private readonly ILogger<OAuthCallbackController> _logger;
 
-        public OAuthCallbackController(ISlackApiClient slackApiClient, IOptionsMonitor<SlackOptions> optionsAccessor)
+        public OAuthCallbackController(ISlackApiClient slackApiClient, IOptionsMonitor<SlackOptions> optionsAccessor, ILogger<OAuthCallbackController> logger)
         {
             _slackApiClient = slackApiClient;
             _optionsAccessor = optionsAccessor;
+            _logger = logger;
         }
 
         [Route("slack/callback")]
         public async Task<IActionResult> Callback(string code)
         {
+            _logger.LogInformation("OAuth callback called with code {Code}", code);
+
             var options = _optionsAccessor.CurrentValue.OAuth;
 
             if (options == null || !options.IsValid())
+            {
+                _logger.LogWarning("Ignoring OAuth callback. No OAuth settings set");
+
                 return Content("Cannot install. No OAuth settings set");
+            }
 
             try
             {
                 // If we want to use the access token we'll need to store it
-                await _slackApiClient.GetOAuthTokenAsync(new OAuthTokenRequest
+                var response = await _slackApiClient.GetOAuthTokenAsync(new OAuthTokenRequest
                 {
                     ClientId = options.ClientId,
                     ClientSecret = options.ClientSecret,
@@ -37,10 +47,14 @@
                     Code = code
                 });
 
+                _logger.LogInformation("OAuth callback complete. OAuth token retrieved");
+
                 return Content("Installed!");
             }
-            catch
+            catch (Exception ex)
             {
+                _logger.LogError(ex, "Failed to get OAuth token");
+
                 return Content("Failed to install :(");
             }
         }
