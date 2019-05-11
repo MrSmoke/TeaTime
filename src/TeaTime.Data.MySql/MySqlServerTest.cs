a56fd2eaf66e58abd40de0467cd1009ab481e7a5
MrSmoke <syco711@gmail.com>
2019-05-11 17:36:37 +1000

    log successful mysql check

--- src/TeaTime.Data.MySql/MySqlServerTest.cs
+++ src/TeaTime.Data.MySql/MySqlServerTest.cs
@@ -9,7 +9,7 @@
         private readonly IMySqlConnectionFactory _connectionFactory;
         private readonly ILogger<MySqlServerTestStartupAction> _logger;
 
-        public string Name => "MySql Server Connection Test";
+        public string Name => "MySQL Server Connection Test";
 
         public MySqlServerTestStartupAction(IMySqlConnectionFactory connectionFactory, ILogger<MySqlServerTestStartupAction> logger)
         {
@@ -23,10 +23,12 @@
             {
                 using (var conn = _connectionFactory.GetConnection())
                     conn.Open();
+
+                _logger.LogInformation("MySQL connectivity OK!");
             }
             catch(Exception ex)
             {
-                _logger.LogError(ex, "Failed to connect to MySql");
+                _logger.LogError(ex, "Failed to connect to MySQL");
 
                 throw;
             }
