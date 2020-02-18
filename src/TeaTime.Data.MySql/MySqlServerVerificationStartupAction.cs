8b9f524ceb3f66315def512445d07e70b86f9a59
MrSmoke <syco711@gmail.com>
2020-02-19 00:08:28 +1100

    Upgrading project to aspnetcore 3.1

--- src/TeaTime.Data.MySql/MySqlServerVerificationStartupAction.cs
+++ src/TeaTime.Data.MySql/MySqlServerVerificationStartupAction.cs
@@ -22,20 +22,19 @@
         {
             try
             {
-                using (var conn = _connectionFactory.GetConnection())
+                using var conn = _connectionFactory.GetConnection();
+
+                conn.Open();
+
+                _logger.LogInformation("MySQL connectivity OK!");
+
+                var evolve = new Evolve(conn, msg => _logger.LogInformation(msg))
                 {
-                    conn.Open();
+                    EmbeddedResourceAssemblies = new[] {typeof(MySqlServerVerificationStartupAction).Assembly},
+                    IsEraseDisabled = true
+                };
 
-                    _logger.LogInformation("MySQL connectivity OK!");
-
-                    var evolve = new Evolve(conn, msg => _logger.LogInformation(msg))
-                    {
-                        EmbeddedResourceAssemblies = new[] {typeof(MySqlServerVerificationStartupAction).Assembly},
-                        IsEraseDisabled = true
-                    };
-
-                    evolve.Migrate();
-                }
+                evolve.Migrate();
             }
             catch(Exception ex)
             {
@@ -44,7 +43,5 @@
                 throw;
             }
         }
-
-
     }
 }
--- src/TeaTime/Startup.cs
+++ src/TeaTime/Startup.cs
@@ -14,9 +14,9 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.HttpOverrides;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
     using Slack;
 
@@ -63,8 +63,7 @@
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            var mvcBuilder = services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var mvcBuilder = services.AddRazorPages();
 
             services.AddTransient<IStartupFilter, StartupActionFilter>();
 
@@ -97,7 +96,7 @@
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -115,7 +114,10 @@
             });
 
             app.UseStaticFiles();
-            app.UseMvc();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => endpoints.MapRazorPages());
         }
     }
 }
