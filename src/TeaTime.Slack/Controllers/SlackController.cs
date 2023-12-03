62dc876677d4856230fddb0acf696b20db43e7c5
Cameron <709976+MrSmoke@users.noreply.github.com>
2023-12-03 16:28:31 +1100

    Remove unused import

--- src/TeaTime.Slack/Controllers/SlackController.cs
+++ src/TeaTime.Slack/Controllers/SlackController.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Text.Json;
     using System.Threading.Tasks;
     using CommandRouter;
     using CommandRouter.Exceptions;
