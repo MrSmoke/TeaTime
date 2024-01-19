e43c051abf663a88fd72febd5676af562c090144
Cameron <709976+MrSmoke@users.noreply.github.com>
2024-01-19 21:51:01 +1100

    init for event timestamp

--- src/TeaTime.Common/Abstractions/Event.cs
+++ src/TeaTime.Common/Abstractions/Event.cs
@@ -1,11 +1,10 @@
-﻿namespace TeaTime.Common.Abstractions
-{
-    using System;
-    using System.Collections.Generic;
+﻿namespace TeaTime.Common.Abstractions;
 
-    public abstract record Event : IEvent
-    {
-        public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.UtcNow;
-        public Dictionary<string, object> State { get; set; } = new();
-    }
+using System;
+using System.Collections.Generic;
+
+public abstract record Event : IEvent
+{
+    public DateTimeOffset Timestamp { get; init; } = DateTimeOffset.UtcNow;
+    public Dictionary<string, object> State { get; init; } = new();
 }
