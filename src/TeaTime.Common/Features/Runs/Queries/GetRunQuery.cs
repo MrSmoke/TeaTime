4555dbcc2fd46c070e86b0c180134ca25763ccba
Cameron <709976+MrSmoke@users.noreply.github.com>
2024-01-19 20:15:41 +1100

    Add GetRunQuery

---
+++ src/TeaTime.Common/Features/Runs/Queries/GetRunQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace TeaTime.Common.Features.Runs.Queries;
+
+using Abstractions;
+using Models.Data;
+
+public class GetRunQuery : IQuery<Run?>
+{
+    public long RunId { get; }
+
+    public GetRunQuery(long runId)
+    {
+        RunId = runId;
+    }
+}
