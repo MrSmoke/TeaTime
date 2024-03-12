23ec123e3f0db5c70eba32ad9d2b5318dcfbf30b
Cameron <709976+MrSmoke@users.noreply.github.com>
2024-03-12 23:51:31 +1100

    use ISlackAuthenticationService instead of the client directly

--- src/TeaTime.Slack/Controllers/OAuthCallbackController.cs
+++ src/TeaTime.Slack/Controllers/OAuthCallbackController.cs
@@ -3,64 +3,40 @@
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
-    using Client;
     using Common.Collections;
-    using Configuration;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
-    using Microsoft.Extensions.Options;
-    using Models.Requests;
     using Models.ViewModels;
 
-    public class OAuthCallbackController : Controller
+    public class OAuthCallbackController(
+        ISlackAuthenticationService slackAuthenticationService,
+        ILogger<OAuthCallbackController> logger,
+        IDistributedHash hash)
+        : Controller
     {
-        private readonly ISlackApiClient _slackApiClient;
-        private readonly IOptionsMonitor<SlackOptions> _optionsAccessor;
-        private readonly ILogger<OAuthCallbackController> _logger;
-        private readonly IDistributedHash _hash;
-
-        public OAuthCallbackController(ISlackApiClient slackApiClient,
-            IOptionsMonitor<SlackOptions> optionsAccessor,
-            ILogger<OAuthCallbackController> logger,
-            IDistributedHash hash)
-        {
-            _slackApiClient = slackApiClient;
-            _optionsAccessor = optionsAccessor;
-            _logger = logger;
-            _hash = hash;
-        }
-
         [Route("slack/callback")]
         public async Task<IActionResult> Callback(string code)
         {
             if (string.IsNullOrWhiteSpace(code))
                 return NotFound();
 
-            _logger.LogInformation("OAuth callback called with code {Code}", code);
+            logger.LogInformation("OAuth callback called with code {Code}", code);
 
-            var options = _optionsAccessor.CurrentValue.OAuth;
-
-            if (options == null || !options.IsValid())
+            if (!slackAuthenticationService.OAuthEnabled())
             {
-                _logger.LogWarning("Ignoring OAuth callback. No OAuth settings set");
+                logger.LogWarning("Ignoring OAuth callback. No OAuth settings set");
                 return View(OAuthCallbackViewModel.Error("not_configured"));
             }
 
             try
             {
                 // If we want to use the access token we'll need to store it
-                var response = await _slackApiClient.GetOAuthTokenAsync(new OAuthTokenRequest
-                {
-                    ClientId = options.ClientId!,
-                    ClientSecret = options.ClientSecret!,
-                    RedirectUri = options.RedirectUri!,
-                    Code = code
-                });
+                var response = await slackAuthenticationService.GetOAuthTokenAsync(code);
 
                 // Check response is successful or not (according to slack)
                 if (!response.IsSuccess)
                 {
-                    _logger.LogError("Failed to install into slack channel. Error: {ErrorCode}", response.Error);
+                    logger.LogError("Failed to install into slack channel. Error: {ErrorCode}", response.Error);
 
                     // set the error code for the view
                     return View(OAuthCallbackViewModel.Error("slack:" + response.Error));
@@ -69,11 +45,11 @@
                 // Validate the response to make sure we have all the data
                 if (!response.ValidateProperties())
                 {
-                    _logger.LogWarning("Response is missing data {@Response}", response.ToLogObject());
+                    logger.LogWarning("Response is missing data {@Response}", response.ToLogObject());
                     return View(OAuthCallbackViewModel.Error("installation_error"));
                 }
 
-                _logger.LogInformation("TeaTime installed in team {TeamId} ({TeamName}) with scope {Scope}",
+                logger.LogInformation("TeaTime installed in team {TeamId} ({TeamName}) with scope {Scope}",
                     response.TeamId, response.TeamName, response.Scope);
 
                 var fields = new List<HashEntry>
@@ -90,7 +66,7 @@
                     if (string.IsNullOrWhiteSpace(incomingWebhook.ChannelId) ||
                         string.IsNullOrWhiteSpace(incomingWebhook.Url))
                     {
-                        _logger.LogWarning("Incoming WebHook is missing data");
+                        logger.LogWarning("Incoming WebHook is missing data");
                     }
                     else
                     {
@@ -99,13 +75,13 @@
                     }
                 }
 
-                await _hash.SetAsync("slack:" + response.TeamId, fields);
+                await hash.SetAsync("slack:" + response.TeamId, fields);
 
                 return View(OAuthCallbackViewModel.Ok(response.TeamName));
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Failed to get OAuth token");
+                logger.LogError(ex, "Failed to get OAuth token");
                 return View(OAuthCallbackViewModel.Error("unknown"));
             }
         }
