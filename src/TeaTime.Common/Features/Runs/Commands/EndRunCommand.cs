94c0c5c9b4a92a66a81cb97396cf30651d33cac2
Cameron <709976+MrSmoke@users.noreply.github.com>
2024-03-12 21:12:52 +1100

    Add the entire Run object to EndRunCommand
    Avoids extra db calls

--- src/TeaTime.Common/Features/Runs/Commands/EndRunCommand.cs
+++ src/TeaTime.Common/Features/Runs/Commands/EndRunCommand.cs
@@ -1,11 +1,11 @@
-﻿namespace TeaTime.Common.Features.Runs.Commands;
+namespace TeaTime.Common.Features.Runs.Commands;
 
 using System.Collections.Generic;
 using Abstractions;
+using Models.Data;
 using Models.Domain;
 
 /// <summary>
 /// The command to end a run
 /// </summary>
-public record EndRunCommand(long RunId, long RoomId, long UserId, IEnumerable<OrderModel> Orders)
-    : BaseCommand, IUserCommand;
+public record EndRunCommand(Run Run, long UserId, IEnumerable<OrderModel> Orders) : BaseCommand, IUserCommand;
