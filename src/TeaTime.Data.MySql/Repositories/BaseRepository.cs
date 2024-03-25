9a82ad79c3598768b2994fd712ae660def3c6276
Cameron <709976+MrSmoke@users.noreply.github.com>
2024-03-25 22:43:34 +1100

    CancellatonToken support in BaseRepository

--- src/TeaTime.Data.MySql/Repositories/BaseRepository.cs
+++ src/TeaTime.Data.MySql/Repositories/BaseRepository.cs
@@ -2,6 +2,7 @@ namespace TeaTime.Data.MySql.Repositories;
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
 using Factories;
@@ -9,30 +10,44 @@ using MySqlConnector;
 
 public abstract class BaseRepository(IMySqlConnectionFactory factory)
 {
-    protected Task<int> ExecuteAsync(string sql, object obj)
+    protected Task<int> ExecuteAsync(string sql, object obj, CancellationToken cancellationToken = default)
     {
-        return GetConnection(conn => conn.ExecuteAsync(sql, obj));
+        var command = CreateCommandDefinition(sql, obj, CommandFlags.Buffered, cancellationToken);
+        return GetConnection(conn => conn.ExecuteAsync(command));
     }
 
-    protected Task<T?> SingleOrDefaultAsync<T>(string sql, object? obj = null)
+    protected Task<T?> SingleOrDefaultAsync<T>(string sql, object? obj = null, CancellationToken cancellationToken = default)
     {
-        return GetConnection(conn => conn.QuerySingleOrDefaultAsync<T>(sql, obj));
+        var command = CreateCommandDefinition(sql, obj, cancellationToken: cancellationToken);
+        return GetConnection(conn => conn.QuerySingleOrDefaultAsync<T>(command));
     }
 
-    protected Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? obj = null)
+    protected Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? obj = null, CancellationToken cancellationToken = default)
     {
-        return GetConnection(conn => conn.QueryFirstOrDefaultAsync<T>(sql, obj));
+        var command = CreateCommandDefinition(sql, obj, cancellationToken: cancellationToken);
+        return GetConnection(conn => conn.QueryFirstOrDefaultAsync<T>(command));
     }
 
-    protected Task<IEnumerable<T>> QueryAsync<T>(string sql, object? obj = null)
+    protected Task<IEnumerable<T>> QueryAsync<T>(string sql, object? obj = null, CancellationToken cancellationToken = default)
     {
-        return GetConnection(conn => conn.QueryAsync<T>(sql, obj));
+        var command = CreateCommandDefinition(sql, obj, cancellationToken: cancellationToken);
+        return GetConnection(conn => conn.QueryAsync<T>(command));
+    }
+
+    private static CommandDefinition CreateCommandDefinition(string sql,
+        object? obj = null,
+        CommandFlags commandFlags = CommandFlags.None,
+        CancellationToken cancellationToken = default)
+    {
+        return new CommandDefinition(commandText: sql,
+            parameters: obj,
+            flags: commandFlags,
+            cancellationToken: cancellationToken);
     }
 
     private async Task<T> GetConnection<T>(Func<MySqlConnection, Task<T>> func)
     {
         await using var conn = factory.GetConnection();
-
         return await func(conn);
     }
 }
