badd82c024aef88078aaefd0e348543005438c13
Cameron <709976+MrSmoke@users.noreply.github.com>
2024-11-17 14:13:19 +1100

    The previos db migration did not work for mariadb
    This fixes that, however a manual db migration will need to be run

--- sql/V0_3_1__Add_soft_delete.sql
+++ sql/V0_3_1__Add_soft_delete.sql
@@ -1,11 +1,13 @@
 alter table `options`
     add column deleted boolean default 0,
     add column deletedDate datetime default null,
+    add column activeName VARCHAR(40) AS (IF(deleted = 0, name, NULL)),
     drop index `idx_options_name`,
-    add unique `idx_options_name` (`groupId`, `name`, (IF(deleted,null,1)));
+    add unique `idx_options_name` (`groupId`, `activeName`);
 
 alter table `option_groups`
     add column deleted boolean default 0,
     add column deletedDate datetime default null,
+    add column activeName VARCHAR(40) AS (IF(deleted = 0, name, NULL)),
     drop index `idx_optiongroups_name`,
-    add unique `idx_optiongroups_name` (`roomId`, `name`, (IF(deleted,null,1)));
+    add unique `idx_optiongroups_name` (`roomId`, `activeName`);
